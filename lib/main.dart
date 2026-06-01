49be2f9056e4a7672cbf84fca385459f14c2cb9d
RayyansStuff <rayyanirfanshah05@gmail.com>
2026-06-01 12:41:58 +0700

    Profile page

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,15 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:device_preview/device_preview.dart';
-import 'package:greengymapp/Pages/LandingPage1.dart';
-
+// import 'package:greengymapp/Pages/LandingPage1.dart';
+import 'package:greengymapp/Pages/profilePage.dart';
 
 void main() => runApp(
-      DevicePreview(
-        enabled: !kReleaseMode, 
-        builder: (context) => const GreenGymApp(), 
-      ),
-    );
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => const GreenGymApp(),
+  ),
+);
 
 class GreenGymApp extends StatelessWidget {
   const GreenGymApp({Key? key}) : super(key: key);
@@ -18,15 +18,13 @@ class GreenGymApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       // Konfigurasi wajib Device Preview
-      useInheritedMediaQuery: true, 
+      useInheritedMediaQuery: true,
       locale: DevicePreview.locale(context),
       builder: DevicePreview.appBuilder,
-      
+
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        scaffoldBackgroundColor: const Color(0xFFE1DCD3), 
-      ),
-      home: const OnboardingScreen(), 
+      theme: ThemeData(scaffoldBackgroundColor: const Color(0xFFE1DCD3)),
+      home: const Profile(),
     );
   }
 }
